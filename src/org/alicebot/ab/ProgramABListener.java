279976a8116d9c28d79ed48024c556c7d029b67b
kwatters <kwatters@kmwllc.com>
2021-08-28 09:39:37 -0400

    javadoc for interface and bumping some versions in the pom to the latest

--- src/org/alicebot/ab/ProgramABListener.java
+++ src/org/alicebot/ab/ProgramABListener.java
@@ -1,9 +1,22 @@
 package org.alicebot.ab;
 
+/**
+ * Base interface to get callbacks when things change on a bot.
+ *
+ */
 public interface ProgramABListener {
   
-    public void onAddCategory(Category category);
+  /**
+   * A callback when a category is added
+   * @param category
+   */
+  public void onAddCategory(Category category);
 
-    public void onChangePredicate(String predicateName, String result);
+  /**
+   * A callback when a predicate is changed for a bot session
+   * @param predicateName
+   * @param result
+   */
+  public void onChangePredicate(String predicateName, String result);
 
 }
